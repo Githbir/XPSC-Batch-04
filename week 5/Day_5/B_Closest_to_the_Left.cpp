a8011510b5087416e642de53f7dbd845841baee1
Tanbir Hasan Jewel <141549622+Githbir@users.noreply.github.com>
2024-04-23 19:34:24 +0600

    Add files via upload

---
+++ week 5/Day_5/B_Closest_to_the_Left.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ll Tan = 1, n, m, x, c, sum, ans;
+void Solve()
+{
+   cin >> n >> m;
+   ll a[n];
+   for (int i = 0; i < n; ++i)
+      cin >> a[i];
+   while (m--)
+   {
+      cin >> x;
+      auto i = upper_bound(a, a + n, x) - a;
+      cout << i << '\n';
+   }
+}
+int main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+   // cin >> Tan;
+   while (Tan--)
+   {
+      c = sum = ans = 0;
+      Solve();
+      cout << '\n';
+   }
+}
