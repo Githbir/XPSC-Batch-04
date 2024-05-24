cdf870cc73ad998eed5dca9fb36282886f49a03f
Tanbir Hasan Jewel <141549622+Githbir@users.noreply.github.com>
2024-05-24 21:11:41 +0600

    Day 7

---
+++ week 9/Day_7/Subarray_Sums_II.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int Test = 1, n, m, k;
+void TANBIR()
+{
+   cin >> n >> k;
+   map<int, int> m;
+   int sum = 0, c = 0, x;
+   m[sum] = 1;
+   for (int i = 0; i < n; i++)
+   {
+      cin >> x;
+      sum += x;
+      c += m[sum - k];
+      m[sum]++;
+   }
+   cout << c << '\n';
+}
+int32_t main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+   TANBIR();
+}
